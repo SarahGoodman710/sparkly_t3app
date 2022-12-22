b2c3975c66c5979ef22d5760995abefa9d35aeba
SarahGoodman710 <sarah.goodman@silvervinesoftware.com>
2022-12-22 14:03:01 -0500

    :lipstick: Change navbar to use user session for image

--- src/components/header/navbar.tsx
+++ src/components/header/navbar.tsx
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { useSession, signOut } from "next-auth/react";
 
 const navigation = [
   { name: "Dashboard", href: "#", current: true },
@@ -13,6 +14,8 @@ function classNames(...classes) {
 }
 
 export default function Navbar() {
+  const { data: session } = useSession();
+
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
@@ -79,7 +82,7 @@ export default function Navbar() {
                       <span className="sr-only">Open user menu</span>
                       <img
                         className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        src={session?.user?.image || ''}
                         alt=""
                       />
                     </Menu.Button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,86 +1,45 @@
-import { signIn } from "next-auth/react";
-import Head from "next/head";
-import { useState } from "react";
-import LoadingDots from "../components/app/loading-dots";
-import Image from "next/image";
-import MyImage from "../images/logo.png";
-import { FcGoogle } from "react-icons/fc";
+import AuthForm from "./login";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { authOptions } from "../pages/api/auth/[...nextauth]";
+import { unstable_getServerSession } from "next-auth/next";
+import { NextPage } from "next";
 
-const pageTitle = "Login";
-const logo = "/favicon.ico";
-const description = "Operations Management App";
+const Home: NextPage = () => {
+  const { data: session } = useSession();
+  const router = useRouter();
 
-export default function Login() {
-  return (
-    <div className="flex min-h-screen flex-col justify-center bg-gray-100 py-12 sm:px-6 lg:px-8">
-      <Head>
-        <title>{pageTitle}</title>
-        <link rel="icon" href={logo} />
-        <link rel="shortcut icon" type="image/x-icon" href={logo} />
-        <link rel="apple-touch-icon" sizes="180x180" href={logo} />
-        <meta name="theme-color" content="#7b46f6" />
+  useEffect(() => {
+    if (session) {
+      router.push("/home");
+    }
+  }, [session, router]);
 
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-        <meta itemProp="name" content={pageTitle} />
-        <meta itemProp="description" content={description} />
-        <meta itemProp="image" content={logo} />
-        <meta name="description" content={description} />
-        <meta property="og:title" content={pageTitle} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={logo} />
-        <meta property="og:type" content="website" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@Elegance" />
-        <meta name="twitter:creator" content="@StevenTey" />
-        <meta name="twitter:title" content={pageTitle} />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={logo} />
-      </Head>
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <Image
-          src={MyImage}
-          alt="Operations Management App"
-          className="mx-auto h-20 w-auto"
-        />
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Operations Management App
-        </h2>
-      </div>
-
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div>
-          <AuthShowcase />
-        </div>
-      </div>
-    </div>
-  );
-}
-
-const AuthShowcase: React.FC = () => {
-  const [loading, setLoading] = useState(false);
-
-  return (
-    <button
-      disabled={loading}
-      onClick={() => {
-        setLoading(true);
-        signIn("google", {callbackUrl: "/"});
-      }}
-      className={`${
-        loading ? "cursor-not-allowed" : ""
-      } h-22 group flex w-full items-center justify-center space-x-5 bg-white py-8 px-4 shadow-md sm:rounded-lg sm:px-10`}
-    >
-      {loading ? (
-        <LoadingDots color="#A8A8A8" />
-      ) : (
-        <div className="flex group-hover:animate-bounce">
-          <FcGoogle size={25} />{" "}
-          <span className="px-2">Continue with Google</span>
-        </div>
-      )}
-    </button>
-  );
+  return <AuthForm />;
 };
+
+export default Home;
+
+export async function getServerSideProps(ctx) {
+  const session = await unstable_getServerSession(
+    ctx.req,
+    ctx.res,
+    authOptions
+  );
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/home",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+}
